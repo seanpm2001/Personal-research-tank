625590c63818be2707321c480ef982c6a0a15b77
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-11-04 13:31:16 -0800

    PROJECT_LANG_V1.py
    An archive of the first version of the project language file for this project.

---
+++ PROJECT_LANG_V1.py
@@ -0,0 +1,4 @@
+# Start of script
+print ("I have chosen Python as the official project language for this project, as it is my best known programming language.")
+''' I have chosen Python as the official project language for this project, as it is my best known programming language. '''
+# End of script
